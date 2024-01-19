d90a7ee9c8ddf5a7a0bb665d9fe2b9401cf231f5
Owen <tianwei2@andrew.cmu.edu>
2024-01-19 00:17:33 -0500

    Implemented namd-id map and foundation food extract

--- FoodData_Central_foundation_food_csv_2023-10-26/preprocess.py
+++ FoodData_Central_foundation_food_csv_2023-10-26/preprocess.py
@@ -1,16 +1,46 @@
 import csv
 
-def read_food(filename):
+
+def get_name_id_map(filename):
   """
-  Reads in the food.csv file
+  Obtains a name to ID map from `food.csv`
   """
   with open(filename) as f:
     reader = csv.reader(f, delimiter=',')
-    rowcount = 0
+    name_id_map = {}
+    is_fst_row = True
     for row in reader:
-      if rowcount == 1125:
-        print(row)
-      rowcount += 1
+      if is_fst_row:
+        is_fst_row = False
+        FDC_ID_IDX, NAME_IDX = row.index('fdc_id'), row.index('description')
+      name, fdc_id = row[NAME_IDX], row[FDC_ID_IDX]
+      if name not in name_id_map:
+        name_id_map[name] = []
+      name_id_map[name].append(fdc_id)
 
-  return rowcount
+  return name_id_map
     
+
+def extract_foundation(filename):
+  """
+  Extracts foundational food from `food.csv`
+  """
+
+  fnd_rows = []
+  with open(filename) as f:
+    reader = csv.reader(f, delimiter=',')
+    is_fst_row = True
+    for row in reader:
+      if is_fst_row:
+        is_fst_row = False
+        DT_IDX, head_row = row.index('data_type'), row
+      if row[DT_IDX] == 'foundation_food':
+        fnd_rows.append(row)
+  
+  with open("extracted_foundation.csv", 'w') as f:
+    writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+    writer.writerow(head_row)
+    for row in fnd_rows:
+      writer.writerow(row)
+
+      
