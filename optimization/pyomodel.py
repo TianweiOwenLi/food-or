b350606b13a9a9aa1d56b9b9a571b4098dede3a4
Owen <tianwei2@andrew.cmu.edu>
2024-03-09 01:31:05 -0500

    Re-organized food model and hooked up scip

--- optimization/pyomodel.py
+++ optimization/pyomodel.py
@@ -1,28 +1,45 @@
-from pyomo.environ import *
+import pyomo.environ as pe
 
-model = AbstractModel()
 
-R_p = NonNegativeReals
+def create_and_solve(filename: str) -> tuple[float, dict[str, int]]:
+  """
+  Constructs and solves MILP model using given data; returns non-zero foods 
+  and their integer quantities as a dictionary.
+  """
 
-model.Foods, model.Nutrients = Set(), Set()
+  R_p, infty = pe.NonNegativeReals, float('inf')
 
-model.qty = Var(model.Foods, within=NonNegativeIntegers)
+  model = pe.AbstractModel()
 
-model.price = Param(model.Foods, within=R_p)
-model.amount = Param(model.Foods, model.Nutrients, within=R_p, default=0.0)
+  model.Foods, model.Nutrients = pe.Set(), pe.Set()
 
-model.nmin = Param(model.Nutrients, within=R_p, default=0.0)
-model.nmax = Param(model.Nutrients, within=R_p, default=float('inf'))
+  model.qtys = pe.Var(model.Foods, within=pe.NonNegativeIntegers, initialize=0)
 
-def abstract_cost_rule(model):
-  return sum(model.price[i] * model.qty[i] for i in model.Foods)
-model.cost = Objective(rule=abstract_cost_rule)
+  model.price = pe.Param(model.Foods, within=R_p, default=infty)
+  model.amount = pe.Param(model.Foods, model.Nutrients, within=R_p, default=0.0)
+  model.nmin = pe.Param(model.Nutrients, within=R_p, default=0.0)
+  model.nmax = pe.Param(model.Nutrients, within=R_p, default=infty)
 
-def nutrient_rule(model, nutrient):
-  value = sum(model.amount[f,nutrient] * model.qty[f] for f in model.Foods)
-  return inequality(model.nmin[nutrient], value, model.nmax[nutrient])
-model.nutrient_limit = Constraint(model.Nutrients, rule=nutrient_rule)
+  def abstract_cost_rule(model):
+    return sum(model.price[i] * model.qtys[i] for i in model.Foods)
+  model.cost = pe.Objective(rule=abstract_cost_rule)
 
-data = DataPortal()
-data.load(filename='../toy_example.json')
-ins = model.create_instance(data)
+  def nutrient_rule(model, nutrient):
+    value = sum(model.amount[f,nutrient] * model.qtys[f] for f in model.Foods)
+    return pe.inequality(model.nmin[nutrient], value, model.nmax[nutrient])
+  model.nutrient_limit = pe.Constraint(model.Nutrients, rule=nutrient_rule)
+
+  data = pe.DataPortal()
+  data.load(filename)
+  instance = model.create_instance(data)
+
+  solver = pe.SolverFactory('scip')
+  solver.solve(instance)
+
+  ret = {}
+  for idx in instance.qtys:
+    qty = round(pe.value(instance.qtys[idx]))
+    if qty:
+      ret[idx] = qty
+  return (pe.value(instance.cost), ret)
+
