5a8de11b61c3d5c3ef4a22c72cd9743231761f1a
Owen <tianwei2@andrew.cmu.edu>
2024-03-09 01:43:39 -0500

    Refactored inspect instacard

--- inspect-instacart-aldi.py
+++ inspect-instacart-aldi.py
@@ -3,6 +3,17 @@ from getpass import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
+def scroll_to_end(driver, elt):
+  script = 'return document.body.scrollHeight'
+  old_y, new_y = -9, driver.execute_script(script)
+  while old_y != new_y:
+    old_y = new_y
+    elt.send_keys(Keys.PAGE_DOWN)
+    time.sleep(1)
+    new_y = driver.execute_script(script)
+
+
 permitted_list = ['produce','dairy','beverages','meat-and-seafood','frozen',
   'baked-goods','3089-deli','3095-prepared-foods','dry-goods-pasta',
   'canned-goods','breakfast-foods']
@@ -23,11 +34,11 @@ login_button.click()
 
 time.sleep(1)
 
-driver.find_element_by_name('email').send_keys(input("email: "))
+driver.find_element_by_name('email').send_keys(input("✉: "))
 pswd_field = driver.find_element_by_name('password')
-pswd_field.send_keys(getpass('password: ') + Keys.TAB + Keys.TAB + Keys.ENTER)
+pswd_field.send_keys(getpass('🔑: ') + Keys.TAB + Keys.TAB + Keys.ENTER)
 
-_ = input("Press ENTER when bypassed reCAPTCHA and loaded store")
+_ = input("Press ENTER after bypassing reCAPTCHA and loading store")
 
 for cat in permitted_list:
   driver.get("https://www.instacart.com/store/aldi/collections/" + cat)
@@ -35,13 +46,7 @@ for cat in permitted_list:
   time.sleep(2.5)
 
   # scroll main page to the bottom to load all entries
-  store_wrap = driver.find_element_by_id('store-wrapper')
-  old_y, new_y = -9, driver.execute_script('return document.body.scrollHeight')
-  while old_y != new_y:
-    old_y = new_y
-    store_wrap.send_keys(Keys.PAGE_DOWN)
-    time.sleep(1)
-    new_y = driver.execute_script('return document.body.scrollHeight')
+  scroll_to_end(driver, driver.find_element_by_id('store-wrapper'))
 
   # find entries with price tag
   for entry in driver.find_elements_by_tag_name('a'):
@@ -54,11 +59,3 @@ for cat in permitted_list:
 with open('items_raw.pkl', 'wb') as f:
   pickle.dump(items_raw, f)
 
-# lines: list[str] = [line.strip() for line in et.split('\n')]
-#   name, price = None, None
-#   for i, line in enumerate(lines):
-#     if (not name) and price and line.isalpha():
-#       name = line
-#     if '$' in line and '.' in line:
-#       price = float(line[1:])
-#     if line.startswith('About')
